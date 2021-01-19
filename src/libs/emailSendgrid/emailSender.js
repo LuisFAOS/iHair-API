0bfb8ddab9988b437140fead238826dfb0d28c31
Luis FAOS <luisfaos.web@gmail.com>
2021-01-19 01:20:59 -0300

    fixing sengrid bug

---
+++ src/libs/emailSendgrid/emailSender.js
@@ -0,0 +1,31 @@
+import sgMail from "@sendgrid/mail"
+import Queue from "../../libs/queueBull/Queue.js"
+
+sgMail.setApiKey(process.env.SENDGRID_KEY)
+
+function sendEmailHandler({
+     functionName,
+     emailDatas
+}) {
+
+     const emailTypes = {
+          sendEmailForValidation: async () => {
+               const emailProps = {
+                    to: emailDatas.email,
+                    from: `iHairSLS <${process.env.EMAIL_IHAIR}>`,
+                    templateId: process.env["EMAIL_CONFIRMATION_TEMPLATE_ID"],
+                    dynamic_template_data: {
+                         dynamicLink: `http://localhost:7070/verify-email?emailToken=${emailDatas.emailToken}&email=${emailDatas.email}`,
+                         userName: emailDatas.name.includes(" ") ?
+                              emailDatas.name.substring(0, emailDatas.name.indexOf(" ")) :
+                              emailDatas.name,
+                    }
+               }
+               sgMail.send(emailProps)
+          }
+     }
+
+     emailTypes[functionName]()
+}
+
+export default sendEmailHandler;
